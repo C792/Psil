88e5dd36ca8d8ba274cb3d9199f5523ce5ab8f41
C792 <ians10845@gmail.com>
2023-04-26 10:15:35 +0900

    Initial Commit

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Hello Streamlit")
+st.write("Hello World")
+number = st.slider("Pick a number", 0, 100)
+color = st.color_picker("Pick a color")
+st.write("임재휘 바보")
